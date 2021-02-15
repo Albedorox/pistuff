18fdc094a4f3261db732eade4c0a7d659ca0f064
Albedorox <57818812+Albedorox@users.noreply.github.com>
2021-02-15 22:04:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,17 @@ if(isset($_POST["ausfuehren"])) {
 
 ?>
 
+<script>
+	function overlay_on(timer) {
+  document.getElementById("overlay").style.display = "block";
+  myVar = setTimeout(overlay_off, timer);
+}
 
+function overlay_off() {
+  document.getElementById("overlay").style.display = "none";
+}
+	</script>
+	
 <div id="main">
 	
 	<div id="header">
