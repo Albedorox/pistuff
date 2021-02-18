8b5dec4f4a43ec0574f808cae5368cecf4d0ac23
Albedorox <57818812+Albedorox@users.noreply.github.com>
2021-02-18 21:43:20 +0100

    Delete js directory

--- js/myjs.js
+++
@@ -1,8 +0,0 @@
-function overlay_on(timer) {
-  document.getElementById("overlay").style.display = "block";
-  myVar = setTimeout(overlay_off, timer);
-}
-
-function overlay_off() {
-  document.getElementById("overlay").style.display = "none";
-}
